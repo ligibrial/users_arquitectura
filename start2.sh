4f32e0257de5d4feed1afd15d4d5994ab4f2d130
Miguel Ballén <migaballengal@unal.edu.co>
2017-11-16 00:57:29 -0500

    Fix: service name

--- start2.sh
+++ start2.sh
@@ -1,4 +1,4 @@
-./rancher-compose --project-name sa-users-ms \
+./rancher-compose --project-name users-ms \
 --url http://192.168.99.100:8080/v1/projects/1a5 \
 --access-key 12DD53C770249D7BDB7B \
 --secret-key mVqhDv7XRvFKSwCPyrmf3qLaC4WLr59adJj3C87g \
